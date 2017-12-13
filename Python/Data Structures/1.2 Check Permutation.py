57aed316e2f7188ada8510e06e6af6e0040d58df
Andrade <andradejr-93@hotmail.com>
2017-12-13 17:01:56 -0300

    1.2 Check Permutation

---
+++ Python/Data Structures/1.2 Check Permutation.py
@@ -0,0 +1,18 @@
+"""
+Given two strings, write a method to decide if one is a permutation of the other.
+"""
+
+def checkPermutation(s1, s2):
+	"""
+	We can sort the strings and compare the results. O(n log n)
+	"""
+	if len(s1) != len(s2):
+		return False
+
+	s1 = sorted(s1)
+	s2 = sorted(s2)
+
+	if s1 == s2:
+		return True
+
+	return False
