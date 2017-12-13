c6f2c2b41d6052e2cef7832565986ef25e06f365
Andrade <andradejr-93@hotmail.com>
2017-12-13 17:41:07 -0300

    1.3 URLify

---
+++ Python/Data Structures/1.3 URLify.py
@@ -0,0 +1,23 @@
+"""
+Write a method to replace all spaces in a string with '%20'. You may assume that the string
+has sufficient space at the end to hold the additional characters, and that you are given 
+the "true" length of the string.
+(Note: If implementing in Java, please use a character array so that you can perform this
+operation in place)
+"""
+
+def URLify(s):
+	"""
+	We can split the string into a list and iterate every word in this list checking if 
+	is a empty space, if not we concatenate the string with the caracter '%20'. O(n)
+	"""
+	l = s.split(' ')
+	s = ''
+
+	for w in l:
+		if w != '' and len(s) != 0:
+			s = s + '%20' + w
+		elif w != '':
+			s = w
+
+	return s
