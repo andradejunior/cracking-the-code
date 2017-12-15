a2c0ace5a5cabbc8a3f1e80043e791d9d9fc4785
Andrade <andradejr-93@hotmail.com>
2017-12-15 16:44:09 -0300

    1.8 Zero Matrix

--- Python/Data Structures/1.8 Zero Matrix.py
+++ Python/Data Structures/1.8 Zero Matrix.py
@@ -1,6 +1,6 @@
 """
 Write an algorithm such that if an element in an MxN matrix is 0, its entire row
-and column are set to 0.
+and column are set to 0. This take O(NM).
 """
 
 def zeroMatrix(matrix):
