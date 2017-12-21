0378d8940ede18e56af320a7d52316933ab683a7
Andrade <andradejr-93@hotmail.com>
2017-12-21 00:42:12 -0300

    2.1 Remove Dups

---
+++ Python/Data Structures/2.3 Delete Middle Node.py
@@ -0,0 +1,5 @@
+"""
+Implement an algorithm to delete a node in the middle(i.e., any node but the first and last node,
+not necessarily the exact middle) of a singly linked list, given
+"""
+class node
