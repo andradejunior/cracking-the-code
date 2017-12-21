354a15cd86718d9eadaf4f309f452698f095da29
Andrade <andradejr-93@hotmail.com>
2017-12-21 01:02:58 -0300

    2.2 Return Kth to Last

--- Python/Data Structures/2.3 Delete Middle Node.py
+++ Python/Data Structures/2.3 Delete Middle Node.py
@@ -2,4 +2,33 @@
 Implement an algorithm to delete a node in the middle(i.e., any node but the first and last node,
 not necessarily the exact middle) of a singly linked list, given
 """
-class node
+class Node:
+	def __init__(self, d):
+		self.data = d
+		self.next = None
+
+	def appendToTail(self, d):
+		end = Node(d)
+		n = self
+		while(n.next is not None):
+			n = n.next
+		n.next = end
+
+	def deleteNode(head, d):
+		n = head
+		if n.data == d:
+			return head.next
+		while n.next is not None:
+			if n.next.data == d:
+				n.next = n.next.next
+				return head
+			n = n.next
+		return head
+
+	def __str__(self):
+		n = self
+		s = '[ '
+		while(n is not None):
+			s = s + str(n.data) + ' '
+			n = n.next
+		return s + ']'
